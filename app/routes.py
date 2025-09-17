6c3c6ad345549894c62c58419ec18df90de924d4
CobraOmega <yashad04@gmail.com>
2025-09-17 15:24:10 +0530

    Made redis visible

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException, BackgroundTasks, Header
+
+from app import celery_app
 from .schemas import SendEmailRequest
 from .utils import send_using_template, send_email_raw
 from .config import settings
 from typing import Optional
 import uuid
 from .tasks import send_email_raw_task, send_template_task
+from celery.result import AsyncResult
 
 router = APIRouter()
 
@@ -27,3 +30,8 @@ async def send_email_endpoint(payload: SendEmailRequest, x_api_key: Optional[str
         send_email_raw_task.delay(payload.to, payload.subject, body, None, payload.from_email)
 
     return {"job_id": job_id, "status": "queued"}
+
+@router.get("/task-status/{job_id}")
+async def get_task_status(job_id: str):
+    res = AsyncResult(job_id, app=celery_app)
+    return {"job_id": job_id, "status": res.status, "result": res.result}
